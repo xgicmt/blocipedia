f9182c58274d1af9df361da868aec1d6be93ffd6
xgicmt <xgicmt@gmail.com>
2015-04-27 10:46:29 -0500

    Create user friendly urls, no plug in

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,4 +2,12 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
   has_many :collaborators
   has_many :users, through: :collaborators
+
+  def slug
+    title.downcase.gsub(/[!@%&"';:,.]/, '').gsub!(/[ ]/, '-')
+  end
+
+  def to_param
+    "#{id}-#{slug}"
+  end
 end
